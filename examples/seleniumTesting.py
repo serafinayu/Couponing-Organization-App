6675c1eb9e91f60235fc8ec4e702494b49174703
Serafina Yu <serafyu@csu.fullerton.edu>
2023-11-14 16:54:48 -0800

    Was able to grab product title from some listings, not all

--- examples/seleniumTesting.py
+++ examples/seleniumTesting.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
+# Serafina Yu
+# @serafinayu
+#
+# This is for me to try out different selenium functions
+#
+
 
 # webdriver module provides all WebDriver implementations like Firefox, Chrome, IE, and Remote
 from selenium import webdriver
@@ -11,20 +17,22 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 # driver,get will navigate to the provided URL and wait until the page is fully loaded before returning control to the rest of the script
 driver.get("https://www.target.com/s?searchTerm=tide&tref=typeahead%7Cterm%7Ctide%7C%7C%7Chistory")
-# Assert an error if "Python" is not in the title 
+# Assert an error if "Target" is not in the title 
 assert "Target" in driver.title
 # Finds the element in the DOM with name "last"
-# Refer here for the rest of the element attributes: https://selenium-python.readthedocs.io/locating-elements.html#locating-elements
+# Refer here for the rest of the element attributes: https://sele    nium-python.readthedocs.io/locating-elements.html#locating-elements
 elem = driver.find_element(By.CLASS_NAME, "iZqUcy")
 print(elem)
 # Process for sending keyboard keys as inputs:
 # Clear any pre-populated text in the input field 
 elem.clear()
 # Send the text "pycon" into the input field 
-elem.send_keys("pycon")
+# elem.send_keys("pycon")
+
 # Hit return so that the form submits
-elem.send_keys(Keys.RETURN)
+# elem.send_keys(Keys.RETURN)
 # After submission of the page, raise this assertion if there are no results
 assert "No results found." not in driver.page_source
 # Close the browser window once done
 driver.close()
+
--- scripts/webscraper.py
+++ scripts/webscraper.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
+# Serafina Yu
+# CPSC 386-02
+# 2023-02-12
+# serafyu@csu.fullerton.edu
+# @serafinayu
+#
+# Project: Webscraper for Target.com
+#
+# This is a webscraper meant to scrape information from Target.com for extreme couponing uses
+#
 
 # Webscraper for Target.com
 # Get the URL to access all participating items for the promotion/deal
@@ -14,23 +24,74 @@
 
 import requests
 from bs4 import BeautifulSoup
+# Module provides all WebDriver implementations like Firefox, Chrome, IE, and Remote
 from selenium import webdriver
+# Allows the use of keyboard keys as inputs
 from selenium.webdriver.common.keys import Keys
+# Allows search of DOM elements by using classes, ids, names, etc
 from selenium.webdriver.common.by import By
+# Lets page fully load before grabbing HTML data
+from selenium.webdriver.support.wait import WebDriverWait
+# Used in conjunction with the wait module: waits for expected conditions
+from selenium.webdriver.support import expected_conditions as EC
+
+""" Main function """
 
 
-URL = "https://www.target.com/pl/130998525?moveTo=432product-list-grid"
-page = requests.get(URL)
+def main():
+    """URL that will be scraped"""
+    URL = "https://www.target.com/s?searchTerm=tide&tref=typeahead%7Cterm%7Ctide%7C%7C%7Chistory"
 
-soup = BeautifulSoup(page.content, "html.parser")
-allListings = soup.find_all("div", class_="dOpyUp")
-# allListings = soup.find_all("div", class_="qwCgKJ")
+    """
+        Beautiful soup - send a get request for this URL and the server will
+        send back & store the HTML data in the page variable.
 
-# Selenium Simple Usage
+        I will be using selenium since beautiful soup cannot wait for the
+        entire page to load before grabbing the elements
+    """
+    # page = requests.get(URL)
+    # soup = BeautifulSoup(page.content, "html.parser")
+    # allListings = soup.find_all("div", class_="qwCgKJ")
+
+    """ Set a Selenium instance of Chrome WebDriver """
+    # Set an instance of Chrome WebDriver
+    driver = webdriver.Chrome()
+    # Navigate to the provided URL and wait til the page is fully loaded before returning control to the rest of the script
+    driver.implicitly_wait(20)
+    driver.get(URL)
+    # Assert an error if "Target" is not in the title
+    assert "Target" in driver.title
+    body = driver.find_element(By.CSS_SELECTOR, 'body')
+    for x in range(0,2):
+        body.send_keys(Keys.PAGE_DOWN)
+    # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    try:
+        elements = driver.find_elements(By.CLASS_NAME, "cbOry")
+        # elements = driver.find_elements(By.CLASS_NAME, "dOpyUp")
+        count = 0
+        for item in elements:
+            print(count, item.text)
+            count += 1
+    # except:
+    #     print("An error occured. Closing.")
+    finally: 
+        # Close the browser window once done
+        driver.close()
+    # try:
+    #     # Find all elements with the following class name
+    #     elem = driver.find_element(By.CLASS_NAME, 'iZqUc')
+    #     print(elem.text)
+    # except:
+        # print("An error occured. Closing.")
+    # After submission of the page, raise this assertion if there are no results
+    # assert "No results found." not in driver.page_source
 
 
-for listing in allListings:
+# async def scrollDown(driver, URL):
+#     await driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    print(listing.text, end="\n"*2)
-    
-print(allListings)
+
+""" Execute main program """
+
+if __name__ == '__main__':
+    main()
